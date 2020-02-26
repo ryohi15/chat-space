ab1c5b481a7efcb21b997836e21b1e452f16615e
ryohi15 <ryo.hitotsu0715@gmail.com>
2020-02-26 12:05:31 +0900

    personal edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
   
@@ -18,11 +20,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
@@ -34,4 +34,7 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
+  def set_group
+    @group = Group.find(params[:id])
 end
