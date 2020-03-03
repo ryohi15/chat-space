b56a45acbfdb49afaed9d3e72d2c55d0ea5636be
ryohi15 <ryo.hitotsu0715@gmail.com>
2020-03-03 20:49:48 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,29 +65,29 @@ $(function(){
     });
   })
 
-  var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0){
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.main_chat__message-list').append(insertHTML);
-        $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  }
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
+  // var reloadMessages = function() {
+  //   var last_message_id = $('.message:last').data("message-id");
+  //   $.ajax({
+  //     url: "api/messages",
+  //     type: 'get',
+  //     dataType: 'json',
+  //     data: {id: last_message_id}
+  //   })
+  //   .done(function(messages) {
+  //     if (messages.length !== 0){
+  //       var insertHTML = '';
+  //       $.each(messages, function(i, message) {
+  //         insertHTML += buildHTML(message)
+  //       });
+  //       $('.main_chat__message-list').append(insertHTML);
+  //       $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+  //     }
+  //   })
+  //   .fail(function() {
+  //     alert('error');
+  //   });
+  // }
+  // if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  //   setInterval(reloadMessages, 7000);
+  // }
 });
