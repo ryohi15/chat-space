0a458691ae94dd02050f90b058f68c1e6ee093ca
ryohi15 <ryo.hitotsu0715@gmail.com>
2020-03-03 17:07:32 +0900

    ummmn

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -18,6 +18,7 @@ $(function(){
         </div>
         <img src=${message.image} >
       </div>`
+      return html;
     } else {
       var html =
        `<div class="message" data-message-id = "${message.id}" >
@@ -29,8 +30,8 @@ $(function(){
               ${message.created_at}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
+          <div class="text">
+            <p class="text__lower-message__content">
               ${message.content}
             </p>
           </div>
@@ -53,6 +54,7 @@ $(function(){
     })
      .done(function(data){
        var html = buildHTML(data);
+       console.log(html)
        $('.main_chat__message-list').append(html);    
        $('.main_chat__message-list').animate({ scrollTop:$('.main_chat__message-list')[0].scrollHeight});  
        $('form')[0].reset();
